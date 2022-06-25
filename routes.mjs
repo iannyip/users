653f24b9780e94750ce3a4e3968b80d2c3ffbecb
iannyip <ianyipsh@gmail.com>
2022-06-25 11:33:43 +0800

    basic structure of routes.mjs filled out

--- routes.mjs
+++ routes.mjs
@@ -1,7 +1,18 @@
 // 1. Import db model from models directory
+import db from "./models/index.mjs";
 
 // 2. Import controllers from controllers directory
+import initUserController from "./controllers/userController.mjs";
 
 // 3. Create bindRoutes function
+const bindRoutes = (app) => {
+  const userController = initUserController(db);
+
+  // Routes
+  app.get("/", (req, res) => {
+    response.send("you are in root");
+  });
+};
 
 // 4. Export bindRoutes function
+export default bindRoutes;
