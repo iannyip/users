605a518fed4c4fb98f5924e0882f45d2e56ed266
iannyip <ianyipsh@gmail.com>
2022-06-25 10:13:58 +0800

    added skeleton models/index.mjs for db object

---
+++ models/index.mjs
@@ -0,0 +1,11 @@
+// 1. Import NPM libraries
+
+// 2. Import config for database to initialize Sequelize obj
+
+// 3. Import models
+
+// 4. Initialize sequelize object
+
+// 5. Initialize an object for each model. Store as attribute of db object
+
+// 6. Export default db
