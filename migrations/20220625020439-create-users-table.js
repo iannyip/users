15fb6b162a71de92d149d45f7f421bd4ae3c2f7a
iannyip <ianyipsh@gmail.com>
2022-06-25 10:08:37 +0800

    added structure of users table

--- migrations/20220625020439-create-users-table.js
+++ migrations/20220625020439-create-users-table.js
@@ -1,21 +1,50 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      first_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      last_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      birthday: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      gender: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      shirt_size: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      net_worth: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
 };
