bf60e91d04d56ae2109fe86e5bad3cd8fc401a54
iannyip <ianyipsh@gmail.com>
2022-06-25 11:50:15 +0800

    completed express app with minimal middleware

--- index.mjs
+++ index.mjs
@@ -2,15 +2,28 @@
 // || yarn start
 
 // 1a. Import libraries
+import express from "express";
+import cookieParser from "cookie-parser";
+import methodOverride from "method-override";
 
 // 1b. Import bindRoutes from ./routes.mjs
+import bindRoutes from "./routes.mjs";
 
 // 2. Variables
 
 // 3. Initialize express
+const app = express();
 
 // 4. Middleware
+const mwLine = (request, response, next) => {
+  console.log("-----------------------------------");
+  next();
+};
 
 // 5. bindRoutes to app
+bindRoutes(app);
 
 // 6. Start express server
+console.log("Starting server...");
+const PORT = process.env.PORT || 3004;
+app.listen(PORT);
