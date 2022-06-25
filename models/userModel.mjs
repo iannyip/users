1906c1cfdf198bd309058077a9444d1531ea7574
iannyip <ianyipsh@gmail.com>
2022-06-25 10:11:25 +0800

    added userModel"

---
+++ models/userModel.mjs
@@ -0,0 +1,46 @@
+export default function initUserModel(sequelize, DataTypes) {
+  return sequelize.define(
+    "user",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      birthday: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      gender: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      shirtSize: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      netWorth: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    { underscored: true }
+  );
+}
