c7b68e0998669ea54fb236fa3dbb1d1ac1234b90
iannyip <ianyipsh@gmail.com>
2022-06-25 11:35:53 +0800

    added skeleton root file index.mjs

---
+++ index.mjs
@@ -0,0 +1,16 @@
+// ROOT FILE -- ENTRY POINT
+// || yarn start
+
+// 1a. Import libraries
+
+// 1b. Import bindRoutes from ./routes.mjs
+
+// 2. Variables
+
+// 3. Initialize express
+
+// 4. Middleware
+
+// 5. bindRoutes to app
+
+// 6. Start express server
