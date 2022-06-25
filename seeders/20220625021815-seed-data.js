834aca98e81311be19807b568baa279b7aeee9cc
iannyip <ianyipsh@gmail.com>
2022-06-25 11:05:29 +0800

    added seed data

--- seeders/20220625021815-seed-data.js
+++ seeders/20220625021815-seed-data.js
@@ -5,6 +5,21 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     const userList = [];
 
+    for (let i = 0; i < 100; i++) {
+      const sizeArr = ["S", "M", "L"];
+      userList.push({
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        email: faker.internet.email(),
+        birthday: faker.date.birthdate({ min: 18, max: 40, mode: "age" }),
+        gender: faker.name.gender(),
+        shirt_size: sizeArr[Math.floor(Math.random() * sizeArr.length)],
+        net_worth: Math.floor(Math.random() * 1000000),
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+
     let users = await queryInterface.bulkInsert("users", userList, {
       returning: true,
     });
