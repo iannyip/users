593d78320e66a80482005e56a281c6af1fd256ff
iannyip <ianyipsh@gmail.com>
2022-06-30 21:04:43 +0800

    updated route.mjs with adminUserController. set up first route

--- routes.mjs
+++ routes.mjs
@@ -3,10 +3,12 @@ import db from "./models/index.mjs";
 
 // 2. Import controllers from controllers directory
 import initUserController from "./controllers/userController.mjs";
+import initAdminUserController from "./controllers/adminUserController.mjs";
 
 // 3. Create bindRoutes function
 const bindRoutes = (app) => {
   const userController = initUserController(db);
+  const adminUserController = initAdminUserController(db);
 
   // Routes
   app.get("/", (request, response) => {
@@ -17,6 +19,8 @@ const bindRoutes = (app) => {
   app.get("/users/:id", userController.show);
   app.put("/users/:id", userController.update);
   app.post("/users/new", userController.create);
+
+  app.get("admin/users", adminUserController.index);
 };
 
 // 4. Export bindRoutes function
