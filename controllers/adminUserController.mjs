cc6fd940eea83beaf0bed304c9e4e77f5fa695bf
iannyip <ianyipsh@gmail.com>
2022-06-30 21:19:35 +0800

    table with all raw data rendered

--- controllers/adminUserController.mjs
+++ controllers/adminUserController.mjs
@@ -4,7 +4,8 @@ export default function initAdminUserController(db) {
   // 1. Route: /admin/users
   const index = async (request, response) => {
     try {
-      response.render("allUsers");
+      const allUserData = await db.User.findAll();
+      response.render("allUsers", { allUserData, moment });
     } catch (error) {
       console.log(error);
     }
